f37531fbf7d44efe80da4ed350271f1bda63a33c
james vanstrahlen <jamesmerchan@hotmail.com>
2020-11-12 22:40:44 -0500

    OctavoCommit

--- Entidad/Pago.cs
+++ Entidad/Pago.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Entidad
 {
     public class Pago
@@ -10,7 +12,11 @@ namespace Entidad
         public int TerceroID { get; set; }
         public String TipoPago { get; set; }
         public DateTime FechaPago { get; set; }
+        
+        [Column(TypeName = "decimal(12,2)")]
         public decimal ValorPago { get; set; }
+
+        [Column(TypeName = "decimal(12,2)")]
         public decimal IVA { get; set; }
 
         
