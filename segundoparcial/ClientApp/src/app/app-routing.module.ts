118fa2a076fcc6385b84a7cf810ca9ee6eb18008
james vanstrahlen <jamesmerchan@hotmail.com>
2020-11-13 00:09:10 -0500

    DecimoTercero Commit

--- segundoparcial/ClientApp/src/app/app-routing.module.ts
+++ segundoparcial/ClientApp/src/app/app-routing.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PagoRegistroComponent } from './tercero/pago-registro/pago-registro.component';
+import { PagoConsultaComponent } from './tercero/pago-consulta/pago-consulta.component';
+import { HomeComponent } from './home/home.component';
+import { TerceroRegistroComponent } from './tercero/tercero-registro/tercero-registro.component';
 
+const routes : Routes = [
+  {
+    path:'pagoRegistro',
+    component: PagoRegistroComponent
+  }
+  ,
+  {
+    path:'pagoConsulta',
+    component: PagoConsultaComponent
+  }
+  ,
+  {
+    path:'',
+    component: HomeComponent
+  }
+  ,
+  {
+    path:'terceroRegistro',
+    component: TerceroRegistroComponent
+  }
 
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
--- segundoparcial/ClientApp/src/app/app.module.ts
+++ segundoparcial/ClientApp/src/app/app.module.ts
@@ -11,15 +11,33 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { AppRoutingModule } from './app-routing.module';
 
+//bootstrap
+import {NgbModule }from '@ng-bootstrap/ng-bootstrap';
+import { AlertModalComponent } from './@base/alert-modal/alert-modal.component';
+import { TerceroRegistroComponent } from './tercero/tercero-registro/tercero-registro.component';
+import { PagoRegistroComponent } from './tercero/pago-registro/pago-registro.component';
+import { TerceroConsultaComponent } from './tercero/tercero-consulta/tercero-consulta.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { PagoConsultaComponent } from './tercero/pago-consulta/pago-consulta.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    AlertModalComponent,
+    TerceroRegistroComponent,
+    PagoRegistroComponent,
+    TerceroConsultaComponent,
+    FooterComponent,
+    HeaderComponent,
+    PagoConsultaComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
@@ -30,6 +48,7 @@ import { AppRoutingModule } from './app-routing.module';
 ], { relativeLinkResolution: 'legacy' }),
     AppRoutingModule
   ],
+  entryComponents:[AlertModalComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
