e81e40aef3f3b4712a6cb7810582cce0771c54e9
james vanstrahlen <jamesmerchan@hotmail.com>
2020-11-13 03:15:23 -0500

    DecimoQuinto Commit

--- segundoparcial/ClientApp/src/app/tercero/pago-registro/pago-registro.component.ts
+++ segundoparcial/ClientApp/src/app/tercero/pago-registro/pago-registro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TerceroService } from 'src/app/services/tercero.service';
+import { Tercero } from '../models/tercero';
+import { TerceroRegistroComponent } from '../tercero-registro/tercero-registro.component';
 
 @Component({
   selector: 'app-pago-registro',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagoRegistroComponent implements OnInit {
 
-  constructor() { }
 
+  constructor(private terceroService: TerceroService,private modalService: NgbModal) { }
+
+  
+  Terceros : Tercero[] = [];
   ngOnInit(): void {
+
+  }
+
+  AbrirRegistro(){
+    const consultaBox = this.modalService.open(TerceroRegistroComponent); 
+   
+  }
+
+  Verificar()
+  {
+    this.AbrirRegistro();
   }
 
 }
--- segundoparcial/ClientApp/src/app/tercero/tercero-registro/tercero-registro.component.ts
+++ segundoparcial/ClientApp/src/app/tercero/tercero-registro/tercero-registro.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AlertModalComponent } from 'src/app/@base/alert-modal/alert-modal.component';
+import { TerceroService } from 'src/app/services/tercero.service';
+import { Tercero } from '../models/tercero';
 
 @Component({
   selector: 'app-tercero-registro',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TerceroRegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal,public activeModal: NgbActiveModal,private terceroService : TerceroService ) { }
+
+  @Input() Identificacion : string;
+
+  tercero : Tercero = new Tercero();
 
   ngOnInit(): void {
   }
 
+  add()
+  {
+    this.terceroService.post(this.tercero).subscribe((p) => {
+      if(p != null)
+      {
+        const messageBox = this.modalService.open(AlertModalComponent)
+        messageBox.componentInstance.title = "Proceso terminado";
+        messageBox.componentInstance.message = "Exitoso";
+      }
+    });
+  }
 }
